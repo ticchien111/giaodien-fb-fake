f84416509ade3e6e69893df53467477bb6f033bf
Dương Chí Hướng <87794542+ticchien111@users.noreply.github.com>
2021-12-05 23:52:47 +0700

    Update index.php

--- 18/index.php
+++ 18/index.php
@@ -13,6 +13,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <style>img[src*="https://cdn.000webhost.com/000webhost/logo/footer-powered-by-000webhost-white2.png"] {display: none;}</style>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
